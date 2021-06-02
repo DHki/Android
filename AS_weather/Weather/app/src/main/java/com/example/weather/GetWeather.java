f07f3f9b74a057f7367b4ccb75b44a20c37cb8d6
DHki <kdh101800@gmail.com>
2021-06-03 04:43:16 +0900

    XML parsing

--- AS_weather/Weather/app/src/main/java/com/example/weather/GetWeather.java
+++ AS_weather/Weather/app/src/main/java/com/example/weather/GetWeather.java
@@ -1,77 +1,180 @@
 package com.example.weather;
 
-import android.app.Activity;
-import android.content.Context;
-import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.BufferedInputStream;
+import android.util.Xml;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
-public class GetWeather{
+public class GetWeather extends Thread{
     private String webUrl; // 접속해야 할 url을 저장합니다.
     private String serviceUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst";
     private String serviceKey = "SGekjuNyDUVjKfl26VG%2BSRQEJDGGsqsKvJTNZrZGQWcez4lllhqoKeYfnryztvVBRRWalGeP4ulEvt1Vkao%2FAg%3D%3D";
-
     private String time[]; // 현재 시간에 대한 정보를 저장할 배열입니다.
+    private String content = "";
 
-    private StringBuilder builder; // 네트워크에 접속하고, xml을 저장할 객체입니다.
+    public String tempreture = "";
+    public int status = -1;
+    public String humidity = "";
+    public String precipitation = "";
+    public String windSpeed = "";
 
-    GetWeather(String nx, String ny){
+    private TextView textView;
+    GetWeather(String nx, String ny, TextView textView){
+        this.textView = textView;
+
         time = getCurrentTime();
         webUrl = serviceUrl + "?serviceKey=" + serviceKey + "&numOfRows=10&pageNo=1&base_date=" + time[0] + "&base_time=" + time[1];
         webUrl += "&nx=" + nx + "&ny=" + ny;
     }
 
-    private String[] getCurrentTime(){
-        SimpleDateFormat format1 = new SimpleDateFormat("yyyyMMdd");
-        SimpleDateFormat format2 = new SimpleDateFormat("HHmm");
+    public void run(){
+        connectWeb();
+        try{
+            analyzeData();
+        }catch (Exception e){}
+
+        String content = makeContent();
+        textView.setText(content);
+    }
+
+    private static String[] getCurrentTime(){
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmm");
         Date time = new Date(System.currentTimeMillis());
 
-        String ret[] = {format1.format(time), format2.format(time)};
-        if(ret[1].charAt(2) >= '4'){
-            SimpleDateFormat tmp = new SimpleDateFormat("HH");
-            ret[1] = tmp.format(time) + "00";
-        }
-        else{
-            SimpleDateFormat tmp = new SimpleDateFormat("HH");
-            if(tmp.equals("00")){
-                ret[1] = (tmp + "00");
+        String ret[] = {format.format(time).substring(0, 8), format.format(time).substring(8)};
+
+        if(Integer.parseInt(ret[1].substring(2)) < 40){
+            String clock = ret[1].substring(0, 2);
+            if(clock.equals("00")) {
+                time = new Date();
+                time = new Date(time.getTime() + (1000*60*60*24 - 1));
+
+                ret[0] = format.format(time).substring(0, 8);
+                ret[1] = "2300";
+            }
+            else if(clock.equals("01")){
+                ret[1] = "0000";
             }
             else {
-                ret[1] = Integer.toString(Integer.parseInt(tmp.format(time)) - 1) + "00";
+                int timeTmp = Integer.parseInt(clock) - 1;
+                if(timeTmp >= 10) ret[1] = Integer.toString(timeTmp) + "00";
+                else ret[1] = "0" + Integer.toString(timeTmp) + "00";
             }
         }
+        else ret[1] = ret[1].substring(0, 2) + "00";
 
         return ret;
     }
 
-    public void connectWeb(){
+    private void connectWeb(){
         try {
             URL url = new URL(webUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
+            InputStream is = conn.getInputStream();
 
-            builder = new StringBuilder();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-            String line;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line = "";
 
-            while((line = reader.readLine()) != null) builder.append(line + "\n");
+            while((line = reader.readLine()) != null) content += line;
         }catch (Exception e) {
             e.printStackTrace();
+            content = e.getMessage();
         }
     }
 
-    public void analyzeData(){
+    private void analyzeData()throws XmlPullParserException, IOException {
+        InputStream inputStream = new ByteArrayInputStream(content.getBytes());
+        XmlPullParser parser = null;
 
+        try{
+            parser = Xml.newPullParser();
+            parser.setFeature(XmlPullParser.FEATURE_PROCESS_NAMESPACES, false);
+            parser.setInput(inputStream, null);
+            parser.next();
+        }catch (Exception e){}
+
+        int cnt = 0;
+        while(parser.next() != XmlPullParser.END_DOCUMENT){
+
+            if(parser.getEventType() == XmlPullParser.START_TAG && parser.getName().equals("obsrValue")) {
+
+                parser.next();
+
+                switch (cnt) {
+                    case 0:
+                        status = Integer.parseInt(parser.getText());
+                        break;
+                    case 1:
+                        humidity = parser.getText();
+                        break;
+                    case 2:
+                        precipitation = parser.getText();
+                        break;
+                    case 3:
+                        tempreture = parser.getText();
+                        break;
+                    case 7:
+                        windSpeed = parser.getText();
+                        break;
+                }
+                cnt++;
+            }
+        }
+    }
+
+    private String makeContent(){
+        String tmp = "\n";
+
+        tmp += "온도: " + tempreture + "℃\n\n\n";
+        tmp += "강수 형태: ";
+        switch (status){
+            case 0:
+                tmp += "없음\n\n\n";
+                break;
+            case 1:
+                tmp += "비\n\n\n";
+                break;
+            case 2:
+                tmp += "비/눈\n\n\n";
+                break;
+            case 3:
+                tmp += "눈\n\n\n";
+                break;
+            case 4:
+                tmp += "소나기\n\n\n";
+                break;
+            case 5:
+                tmp += "빗방울\n\n\n";
+                break;
+            case 6:
+                tmp += "빗방울/눈날림\n\n\n";
+                break;
+            case 7:
+                tmp += "눈날림\n\n\n";
+                break;
+            default:
+                tmp += "XML 파싱 에러\n\n\n";
+                break;
+        }
+        tmp += "습도: " + humidity + "%\n\n\n";
+        tmp += "강수량: " + precipitation + "mm\n\n\n";
+        tmp += "풍속: " + windSpeed + "m/s";
+
+        return tmp;
     }
 }
--- AS_weather/Weather/app/src/main/java/com/example/weather/WeatherScreen.java
+++ AS_weather/Weather/app/src/main/java/com/example/weather/WeatherScreen.java
@@ -1,6 +1,7 @@
 package com.example.weather;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +16,6 @@ import java.util.concurrent.TimeUnit;
 
 public class WeatherScreen extends AppCompatActivity {
     ExecutorService service;
-    Executor executer;
 
     GetWeather weather;
 
@@ -25,57 +25,30 @@ public class WeatherScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        service = Executors.newFixedThreadPool(4);
+
         setContentView(R.layout.weather_screen);
 
         setDefault();
-        setText();
     }
 
     private void setDefault(){
-        service = Executors.newFixedThreadPool(4);
-
         index = getIntent().getIntExtra("index", -1);
         location = getResources().getStringArray(R.array.location);
         nx = getResources().getStringArray(R.array.nx);
         ny = getResources().getStringArray(R.array.ny);
 
         // 먼저 새로운 스레드로 인터넷에 연결하도록 해야합니다.
-        weather = new GetWeather(nx[index], ny[index]);
-        executer.execute(new Runnable() {
-            @Override
-            public void run() {
-                weather.connectWeb();
-            }
-        });
+        weather = new GetWeather(nx[index], ny[index], (TextView) findViewById(R.id.weather_content));
+        weather.start();
 
-        // 그 뒤에 weather.analyze()를 한 뒤에 화면에 출력하도록 해야함.
-        try{
-            if(service.awaitTermination(5, TimeUnit.SECONDS)){
-                executer.execute(new Runnable() {
-                    @Override
-                    public void run() {
-                        weather.analyzeData();
-                    }
-                });
-            }
-            else{
-                Toast.makeText(this, "문제가 발생했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
-            }
-        } catch(Exception e) { e.printStackTrace(); }
+        Toast.makeText(this, "Connecting started", Toast.LENGTH_SHORT).show();
+
 
         TextView title = (TextView) findViewById(R.id.title_weather);
         String tmp_title = location[index] + "의 현재 날씨";
         title.setText(tmp_title);
     }
 
-    private void setText(){
-        try{
-            if(service.awaitTermination(5, TimeUnit.SECONDS)){
-
-            }
-            else{
-                Toast.makeText(this, "문제가 발생했습니다. 다시 시도해주세요.", Toast.LENGTH_SHORT).show();
-            }
-        }catch (Exception e){ e.printStackTrace(); }
-    }
+    public void back(View view){ finish(); }
 }
