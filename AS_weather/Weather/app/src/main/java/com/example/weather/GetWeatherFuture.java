b83d9a45c69599cb2f2c4e4c16ae2b6d7c6e284d
DHki <kdh101800@gmail.com>
2021-06-03 13:54:43 +0900

    0603

--- AS_weather/Weather/app/src/main/java/com/example/weather/GetWeatherFuture.java
+++ AS_weather/Weather/app/src/main/java/com/example/weather/GetWeatherFuture.java
@@ -1,40 +1,36 @@
 package com.example.weather;
 
-
-
 import android.util.Xml;
 import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
-
 import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class GetWeatherCurrent extends Thread{
-    private String webUrl; // 접속해야 할 url을 저장합니다.
-    private String serviceUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst";
+public class GetWeatherFuture extends Thread{
+    private String webUrl;
+    private String serviceUrl = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst";
     private String serviceKey = "SGekjuNyDUVjKfl26VG%2BSRQEJDGGsqsKvJTNZrZGQWcez4lllhqoKeYfnryztvVBRRWalGeP4ulEvt1Vkao%2FAg%3D%3D";
-    private String time[]; // 현재 시간에 대한 정보를 저장할 배열입니다.
-    private String content = "";
-
-    private String tempreture = "";
-    private int status = -1;
-    private String humidity = "";
-    private String precipitation = "";
-    private String windSpeed = "";
-
+    private String time[];
+    private String response;
     private TextView textView;
-    GetWeatherCurrent(String nx, String ny, TextView textView){
+
+    private int status = -1;
+    private int sky = -1;
+    private String percent;
+    private String maxTempreture;
+    private String minTempreture;
+
+    GetWeatherFuture(String nx, String ny, TextView textView){
         this.textView = textView;
 
         time = getCurrentTime();
@@ -43,40 +39,59 @@ public class GetWeatherCurrent extends Thread{
     }
 
     public void run(){
-        connectWeb();
         try{
-            analyzeData();
+            connectWeb();
+            //analyzeResponse();
         }catch (Exception e){}
 
-        String content = makeContent();
-        textView.setText(content);
+        //String content = makeContent();
+        //textView.setText(content);
     }
 
-    private static String[] getCurrentTime(){
+    private String[] getCurrentTime(){
         SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmm");
         Date time = new Date(System.currentTimeMillis());
 
         String ret[] = {format.format(time).substring(0, 8), format.format(time).substring(8)};
 
-        if(Integer.parseInt(ret[1].substring(2)) < 40){
-            String clock = ret[1].substring(0, 2);
-            if(clock.equals("00")) {
+        if(Integer.parseInt(ret[1].substring(2)) >= 10) {
+            int tmp = Integer.parseInt(ret[1].substring(0, 2));
+
+            if(tmp >= 23) ret[1] = "2300";
+            else if(tmp >= 20) ret[1] = "2000";
+            else if(tmp >= 17) ret[1] = "1700";
+            else if(tmp >= 14) ret[1] = "1400";
+            else if(tmp >= 11) ret[1] = "1100";
+            else if(tmp >= 8) ret[1] = "0800";
+            else if(tmp >= 5) ret[1] = "0500";
+            else if(tmp >= 2) ret[1] = "0200";
+            else {
                 time = new Date();
                 time = new Date(time.getTime() + (1000*60*60*24 - 1));
 
                 ret[0] = format.format(time).substring(0, 8);
                 ret[1] = "2300";
             }
-            else if(clock.equals("01")){
-                ret[1] = "0000";
-            }
+
+        }
+        else {
+            int tmp = Integer.parseInt(ret[1].substring(0, 2));
+
+            if(tmp >= 21) ret[1] = "2000";
+            else if(tmp >= 18) ret[1] = "1700";
+            else if(tmp >= 15) ret[1] = "1400";
+            else if(tmp >= 12) ret[1] = "1100";
+            else if(tmp >= 9) ret[1] = "0800";
+            else if(tmp >= 6) ret[1] = "0500";
+            else if(tmp >= 3) ret[1] = "0200";
             else {
-                int timeTmp = Integer.parseInt(clock) - 1;
-                if(timeTmp >= 10) ret[1] = Integer.toString(timeTmp) + "00";
-                else ret[1] = "0" + Integer.toString(timeTmp) + "00";
+                time = new Date();
+                time = new Date(time.getTime() + (1000*60*60*24 - 1));
+
+                ret[0] = format.format(time).substring(0, 8);
+                ret[1] = "2300";
             }
         }
-        else ret[1] = ret[1].substring(0, 2) + "00";
 
         return ret;
     }
@@ -90,14 +105,13 @@ public class GetWeatherCurrent extends Thread{
             BufferedReader reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
             String line = "";
 
-            while((line = reader.readLine()) != null) content += line;
+            while((line = reader.readLine()) != null) response += line;
         }catch (Exception e) {
             e.printStackTrace();
-            content = e.getMessage();
         }
     }
 
-    private void analyzeData()throws XmlPullParserException, IOException {
+    /* private void analyzeData() throws XmlPullParserException, IOException {
         InputStream inputStream = new ByteArrayInputStream(content.getBytes());
         XmlPullParser parser = null;
 
@@ -135,46 +149,5 @@ public class GetWeatherCurrent extends Thread{
                 cnt++;
             }
         }
-    }
-
-    private String makeContent(){
-        String tmp = "\n";
-
-        tmp += "온도: " + tempreture + "℃\n\n\n";
-        tmp += "강수 형태: ";
-        switch (status){
-            case 0:
-                tmp += "없음\n\n\n";
-                break;
-            case 1:
-                tmp += "비\n\n\n";
-                break;
-            case 2:
-                tmp += "비/눈\n\n\n";
-                break;
-            case 3:
-                tmp += "눈\n\n\n";
-                break;
-            case 4:
-                tmp += "소나기\n\n\n";
-                break;
-            case 5:
-                tmp += "빗방울\n\n\n";
-                break;
-            case 6:
-                tmp += "빗방울/눈날림\n\n\n";
-                break;
-            case 7:
-                tmp += "눈날림\n\n\n";
-                break;
-            default:
-                tmp += "XML 파싱 에러\n\n\n";
-                break;
-        }
-        tmp += "습도: " + humidity + "%\n\n\n";
-        tmp += "강수량: " + precipitation + "mm\n\n\n";
-        tmp += "풍속: " + windSpeed + "m/s";
-
-        return tmp;
-    }
+    }*/
 }
